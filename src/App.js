22abacdd21441b4123056036c6910957ddc2cd07
margauxlacheze <margaux.lchz@gmail.com>
2023-02-20 15:14:24 +0100

    dynamisation list items

--- src/App.js
+++ src/App.js
@@ -3,37 +3,43 @@ import React, {useState} from 'react';
 import Depenses from "./components/Depenses/Depenses";
 import NouvelleDepense from './components/NouvelleDepense/NouvelleDepense';
 
-const App = () => {
-  const depenses = [
-    {
-      id: 'e1',
-      titre: 'Assurance voiture', 
-      montant: 107.5, 
-      date: new Date(2022, 2, 28)
-    },
-    {
-      id: 'e2',
-      titre: 'Frais vétérinaire', 
-      montant: 355.7, 
-      date: new Date(2021, 9, 15)
-    },
-    {
-      id: 'e3',
-      titre: 'Chauffage', 
-      montant: 87.9, 
-      date: new Date(2020, 11, 5) 
-    },
-    { 
-      id: 'e4',
-      titre: 'Nourriture', 
-      montant: 200.46, 
-      date: new Date(2020, 6, 16)
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    titre: 'Assurance voiture', 
+    montant: 107.5, 
+    date: new Date(2022, 2, 28)
+  },
+  {
+    id: 'e2',
+    titre: 'Frais vétérinaire', 
+    montant: 355.7, 
+    date: new Date(2021, 9, 15)
+  },
+  {
+    id: 'e3',
+    titre: 'Chauffage', 
+    montant: 87.9, 
+    date: new Date(2020, 11, 5) 
+  },
+  {
+    id: 'e4',
+    titre: 'Nourriture', 
+    montant: 200.46, 
+    date: new Date(2020, 6, 16)
+  },
+];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+const App = () => {
+  // passer l'array en état initial
+  const [depenses, setDepenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = depense => {
+    //maj state quand basé sur ancienne version d'un state
+    setDepenses(prevDepenses => {
+      return [depense, ...prevDepenses]
+    }
+    );
   };
   return (
     <div>
